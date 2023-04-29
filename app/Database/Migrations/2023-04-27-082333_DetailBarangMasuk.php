4ca9df181ef0a5c329844c2293f3892d02f14265
Handoko <hd.doko22@gmail.com>
2023-04-29 09:42:26 +0700

    add trigger

--- app/Database/Migrations/2023-04-27-082333_DetailBarangMasuk.php
+++ app/Database/Migrations/2023-04-27-082333_DetailBarangMasuk.php
@@ -38,6 +38,10 @@ class DetailBarangMasuk extends Migration
         ]);
         $this->forge->addKey('det_id', true);
         $this->forge->createTable('detail_barang_masuk');
+        $this->db->query("CREATE TRIGGER `tambahStok` AFTER INSERT ON `detail_barang_masuk` FOR EACH ROW UPDATE barangs SET barangs.brg_stok = barangs.brg_stok + NEW.det_jumlah WHERE barangs.brg_kode = NEW.det_brg_kode");
+        $this->db->query("CREATE TRIGGER `ubahStokKurang` AFTER UPDATE ON `detail_barang_masuk` FOR EACH ROW UPDATE barangs SET barangs.brg_stok = barangs.brg_stok - old.det_jumlah WHERE barangs.brg_kode = old.det_brg_kode");
+        $this->db->query("CREATE TRIGGER `ubahStokTambah` AFTER UPDATE ON `detail_barang_masuk` FOR EACH ROW UPDATE barangs SET barangs.brg_stok = barangs.brg_stok + NEW.det_jumlah WHERE barangs.brg_kode = NEW.det_brg_kode");
+        $this->db->query("CREATE TRIGGER `hapusStok` AFTER DELETE ON `detail_barang_masuk` FOR EACH ROW UPDATE barangs SET barangs.brg_stok = barangs.brg_stok - old.det_jumlah WHERE barangs.brg_kode = old.det_brg_kode");
     }
 
     public function down()
